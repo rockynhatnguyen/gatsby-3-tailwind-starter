1e72e6ff30fcff13040960e38e2e2a9c7419df66
Rocky <rockynhatnguyen@gmail.com>
2021-03-08 21:55:12 -0800

    Update: source

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{css,scss,js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
